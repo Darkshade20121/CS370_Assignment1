9601cfb66f0597f7b85d06f8e82ff64d9c30c291
Andrew Kassab <andrew.kassab@typethink.com>
2021-10-27 15:57:49 -0700

    Finished assignment

--- Question2/question2.py
+++ Question2/question2.py
@@ -12,6 +12,7 @@ def encrypt(key, plaintext):
     encryptor = Cipher(
         algorithms.AES(key),
         modes.CBC(iv),
+        backend
     ).encryptor()
 
     ciphertext = encryptor.update(plaintext) + encryptor.finalize()
@@ -24,9 +25,9 @@ def set_length(word, plaintext, ciphertext):
         stripped_plaintext = plaintext.rstrip('\n')
         word = word.rstrip('\n')
         new_word = word.ljust(16)
-        b = bytes(new_word, 'utf-8')
+        b = bytes(new_word)
         new_cipher = plaintext.ljust(32, '\x00')
-        b_cipher = bytes(new_cipher, 'utf-8')
+        b_cipher = bytes(new_cipher)
 
         x = encrypt(b, b_cipher)
 
@@ -37,10 +38,10 @@ def set_length(word, plaintext, ciphertext):
 def read_dictionary():
     fp = open("dictionary.txt","r")
 
-    plaintext = input("Enter plaintext here: ")
+    plaintext = raw_input("Enter plaintext here: ")
     plaintext = plaintext[0:16]
 
-    ciphertext = input("Enter ciphertext here: ")
+    ciphertext = raw_input("Enter ciphertext here: ")
     ciphertext = ciphertext[0:32]
 
     for x in fp:
--- Question3/question3.py
+++ Question3/question3.py
@@ -1,67 +1,88 @@
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.backends.openssl.backend import backend
-import time
 import random
-
-# digest = hashes.Hash(hashes.SHA256())
-# digest.update(b"abc")
-# digest.update(b"123")
-# a = digest.finalize()
-# print(a)
 
 def weak():
     hits = []
     i = 0
-    while(i < 1000):
-        temp_num = random.randint(0,400)
-        temp_num_ = bin(temp_num)
-        temp_num_ = bytes(temp_num_, 'utf-8')
+    while(i < 5):
+        temp_num = bin(random.getrandbits(24))
+        temp_num_ = bytes(temp_num)
 
-        digest = hashes.Hash(hashes.SHA256())
+        digest = hashes.Hash(hashes.SHA256(), backend)
         digest.update(temp_num_)
         a = digest.finalize()
-        start = time.time()
 
-        temp = 0
         counter = 0
         while(True):
-            temp += 1
-            temp_bin = bin(temp)
-            temp_bin = bytes(temp_bin, 'utf-8')
-            print(temp_bin)
-
-            b_digest = hashes.Hash(hashes.SHA256())
-
+            temp_bin = bin(random.getrandbits(24))
+            temp_bin = bytes(temp_bin)
+            b_digest = hashes.Hash(hashes.SHA256(), backend)
             b_digest.update(temp_bin)
             b = b_digest.finalize()
-            # print("a: " , a)
-            # print("b: ", b)
 
-            if(a==b):
+            if(a[0:6]==b[0:6]):
                 hits.append(counter)
                 break
             counter += 1
         i += 1
-        end=time.time()
+        print "Done with: ", i
 
-    # print("\n\n\n\n\n\n\n\n NO WAY")
-    # print("start: ", start)
-    # print("end: ", end)
-    print("counter: ", counter)
+    print "counter: ", counter
 
     average = Average(hits)
-    print("array: ", hits)
-    print("average: ", average)
-
+    print "Array: ", hits
+    print "Average: ", average
 
 def Average(lst):
     return sum(lst) / len(lst)
 
-# def strong():
-#     i = 0;
-#     while(i <= 104):
+def strong():
+    hits = []
+    array_of_hash = []
+
+    run_times = 0
+    i = 0
+    counter = 0
+    isFound = False
+    while(isFound == False):
+        temp_num = bin(random.getrandbits(24))
+        temp_num_ = bytes(temp_num)
+        digest = hashes.Hash(hashes.SHA256(), backend)
+        digest.update(temp_num_)
+        a = digest.finalize()
+
+        for i in range(len(array_of_hash)):
+                if array_of_hash[i] == a:
+                    # print("Stuck here 3")
+                    isFound = True
+                    return counter
+
+        counter += 1
+        array_of_hash.append(a)
+        run_times += 1
+
+
+def user_input():
+    a = input("Enter 1 for WEAK or 2 for STRONG: ")
+
+    if a == 1:
+        weak()
+    elif a == 2:
+        i = 0
+        hits = []
+        while(i < 100):
+            hits.append(strong())
+            i += 1
+            print "Percent Done: ", i, "%"
+        average = Average(hits)
+        print "Average: ", average
+    else:
+        print "Incorrect input try again."
+        user_input()
 
 
 def main():
-    weak()
+    user_input()
+
 main()
